6bf83741fc7d8de59128a4b7553bede15291aa19
pein <2694041278@qq.com>
2016-08-04 13:50:51 +0800

    this is version 4 moring job

--- app/src/main/java/com/example/pein/siamuniversity/WelcomeActivity.java
+++ app/src/main/java/com/example/pein/siamuniversity/WelcomeActivity.java
@@ -1,5 +1,7 @@
 package com.example.pein.siamuniversity;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,24 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        mHandler.sendEmptyMessageDelayed(GOTO_MAIN_ACTIVITY, 3000);
     }
+    protected final int GOTO_MAIN_ACTIVITY = 0;
+    Handler mHandler = new Handler(new Handler.Callback() {
+
+        public boolean handleMessage(android.os.Message msg) {
+            switch (msg.what) {
+                case GOTO_MAIN_ACTIVITY:
+                    Intent intent = new Intent();
+                    intent.setClass(WelcomeActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                    break;
+                default:
+                    break;
+            }
+            return true;
+
+        }
+    });
 }
