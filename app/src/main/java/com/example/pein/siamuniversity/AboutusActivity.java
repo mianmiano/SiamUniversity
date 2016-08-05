655fdc3389a79e28132e99cac71951bbd7ee57f6
pein <2694041278@qq.com>
2016-08-05 17:33:43 +0800

    5 morning

--- app/src/main/java/com/example/pein/siamuniversity/AboutusActivity.java
+++ app/src/main/java/com/example/pein/siamuniversity/AboutusActivity.java
@@ -7,12 +7,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class AboutusActivity extends AppCompatActivity implements View.OnClickListener {
+public class AboutusActivity extends AppCompatActivity  implements View.OnClickListener {
 
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aboutus);
+
     }
     public void onClick(View v) {
 
