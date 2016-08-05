3e8c0e1025276e09fdff0c2d18a2e59b7a1f5b21
pein <2694041278@qq.com>
2016-08-05 11:12:45 +0800

    5 morning

---
+++ app/src/main/java/com/example/pein/siamuniversity/tryActivity.java
@@ -0,0 +1,14 @@
+package com.example.pein.siamuniversity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class tryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_try);
+    }
+
+}
